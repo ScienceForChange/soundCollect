1ae9044eeab3713743c6eed19fa3646971ccfeb8
Carlos Millan <carlos.millan@scienceforchange.eu>
2023-11-15 13:53:40 +0100

    user authentication

---
+++ api/routes/auth.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Route;
+
+// ENDPOINT - Registro de usuario
+Route::post('/register', [RegisteredUserController::class, 'store'])
+                ->middleware('guest')
+                ->name('register');
+
+// ENDPOINT - Login de usuario
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+                ->middleware('guest')
+                ->name('login');
+
+// ENDPOINT - Envío de link para resetear la contraseña
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+                ->middleware('guest')
+                ->name('password.email');
+
+// ENDPOINT - Reseteo de contraseña
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+                ->middleware('guest')
+                ->name('password.store');
+
+// URL - Al acceder a ella con el id de usuario y un hash válido se valida el usuario y se redirige
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->name('verification.verify');
+
+// ENDPOINT - Para el re-envio de verificacion de email
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+                ->middleware(['auth', 'throttle:6,1'])
+                ->name('verification.send');
+
+// ENDPOINT - Logout
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->middleware('auth')
+                ->name('logout');
