4b7b46304e644c42a9744a98dd88167b73b9bbe5
Carlos Millan <carlos.millan@scienceforchange.eu>
2024-01-22 12:47:51 +0100

    migration of observations

--- api/database/migrations/2024_01_22_112654_create_observations_table.php
+++ api/database/migrations/2024_01_22_112654_create_observations_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('observations', function (Blueprint $table) {
             $table->id();
+            // Screen 1
+            $table->string('audio_param_1')->nullable(); //TODO: afinar
+            $table->string('audio_param_2')->nullable(); //TODO: afinar
+            $table->string('audio_param_3')->nullable(); //TODO: afinar
+            $table->string('audio_param_4')->nullable(); //TODO: afinar
+            // Screen 2
+            $table->json('images')->nullable(); //TODO: afinar
+            // Screen 3
+            $table->string('sound_type')->nullable(); //TODO: afinar
+            $table->string('sound_source')->nullable(); //TODO: afinar
+            $table->string('sound_perception_enviroment')->nullable(); //TODO: afinar
+            $table->text('comments')->nullable(); //TODO: afinar
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
