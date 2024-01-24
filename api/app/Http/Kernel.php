3498600fc56b7b83f9e6978e9ede354ed121f5df
Carlos Millan <carlos.millan@scienceforchange.eu>
2024-01-24 12:48:08 +0100

    remove middleware: EnsureFrontendRequestsAreStateful cos we are not using session based authentication

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -39,7 +39,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // We are not using session based authentication.
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
