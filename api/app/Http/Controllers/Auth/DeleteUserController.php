0aef899f801e47403075dd0f7b5ccbe995b673c2
Carlos Millan <carlos.millan@scienceforchange.eu>
2024-02-16 15:20:49 +0100

    delete user

--- api/app/Http/Controllers/Auth/DeleteUserController.php
+++ api/app/Http/Controllers/Auth/DeleteUserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Traits\ApiResponses;
 use Illuminate\Support\Facades\Auth;
 
 class DeleteUserController
 {
+    use ApiResponses;
     public function __invoke()
     {
         $user = auth()->user();
@@ -13,5 +15,12 @@ class DeleteUserController
         $user->otp()->delete();
 
         $user->delete();
+
+        return $this->success(
+            [
+                'message' => 'User deleted successfully',
+            ],
+            200
+        );
     }
 }
