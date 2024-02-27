c9c34f88f297d05cd764d678a0861e3e6d4693ae
Otari Oboladze <otari.oboladze@scienceforchange.eu>
2024-02-27 13:42:39 +0100

    added L90 and L10 to audio params

--- audio_to_params.py
+++ audio_to_params.py
@@ -2,7 +2,7 @@ import maad
 from maad import spl, sound
 import numpy as np
 
-return_array = []
+response = []
 
 # input sound file
 w, fs = maad.sound.load('Oficina-Z.wav') 
@@ -18,18 +18,31 @@ x =  maad.spl.pressure2dBSPL(abs(p))
 #split x into 1000 arrays, on a sample 1 minute WAV audio it had 2.8 milion values
 splitted_x_into_1000_arrays = np.array_split(x, 1000)
 
-# create a median array
+# create a 1000 values median array
 median_array = []
 for i in splitted_x_into_1000_arrays:
     median_array.append(float(np.mean(i)))
 
-# create a return array
-# add Lmin
-return_array.append('Lmin: ' + str(min(median_array)) + ' dB')
-# add Lmax
-return_array.append('Lmax: ' + str(max(median_array)) + ' dB')
-# add Leq
-return_array.append('Leq: ' + str(maad.spl.pressure2dBSPL(p_rms)) + ' dB')
+# sort the median array to get L90 and L10
+sorted_median_array = sorted(median_array)
+L90 = sorted_median_array[int(len(median_array)*0.1)]
+L10 = sorted_median_array[int(len(median_array)*0.9)]
 
-print(return_array)
+# create a response
+# add Lmin
+response.append('Lmin: ' + str(min(median_array)) + ' dB')
+# add Lmax
+response.append('Lmax: ' + str(max(median_array)) + ' dB')
+# add Leq
+response.append('Leq: ' + str(maad.spl.pressure2dBSPL(p_rms)) + ' dB')
+# add LAeq,T
+response.append('LAeq,T: ' + str(median_array))
+# add L90
+response.append('L90: ' + str(L90) + ' dB')
+# add L10
+response.append('L10: ' + str(L10) + ' dB')
+
+print(response)
+
+
 
