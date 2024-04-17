786e4d371e96c43176efae505470c6e82bab0358
Otari Oboladze <otari.oboladze@scienceforchange.eu>
2024-04-17 14:09:54 +0200

    typo

--- api/database/migrations/2024_04_17_125800_add_autocalibration_column_to_users.php
+++ api/database/migrations/2024_04_17_125800_add_autocalibration_column_to_users.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('observations', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->integer('autocalibration')->default(30);
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('observations', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('autocalibration');
         });
     }
