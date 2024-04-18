7b528b0dee11893ca260dd05f523d980ec757114
Otari Oboladze <otari.oboladze@scienceforchange.eu>
2024-04-18 17:05:59 +0200

    trigger server to start

--- api/routes/api.php
+++ api/routes/api.php
@@ -96,4 +96,3 @@ Route::get('/user/observations', [ObservationController::class, 'userObservation
         // Route::post('/user/autocalibration', \App\Http\Controllers\AutocalibrationController::class)->middleware(['auth:sanctum'])->name('autocalibration.update');
 Route::post('/user/autocalibration', \App\Http\Controllers\AutocalibrationController::class)->middleware(['auth:sanctum'])->name('autocalibration.update');
 
-// trigger
