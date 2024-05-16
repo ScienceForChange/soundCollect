d6b29c4a7b2bd47c319aa8c878c44773d2f1e1d3
Otari Oboladze <otari.oboladze@scienceforchange.eu>
2024-05-16 09:38:00 +0200

    add path to map controller

--- api/app/Http/Resources/MapObservationResource.php
+++ api/app/Http/Resources/MapObservationResource.php
@@ -23,6 +23,7 @@ class MapObservationResource extends JsonResource
             'user_id' => $this->user_id,
             'latitude' => $this->latitude,
             'longitude' => $this->longitude,
+            'path' => $this->path,
         ];
     }
 }
