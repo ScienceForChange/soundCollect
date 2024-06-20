fcd29dea2f9458997b0809cb18662882b35be0a2
Otari Oboladze <otari.oboladze@scienceforchange.eu>
2024-06-20 16:36:42 +0200

    modif migration name for eloquent relationship

--- api/database/migrations/2024_06_19_140000_create_polylines_table.php
+++ api/database/migrations/2024_06_19_140000_create_polylines_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('polyline_observations', function (Blueprint $table) {
+        Schema::create('polylines', function (Blueprint $table) {
 
             $table->id();
 
