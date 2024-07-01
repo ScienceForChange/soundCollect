d96a42b0d86b95f4791e2bd15fb994153f0c9b90
Carlos Millán <carlos.millan@scienceforchange.eu>
2024-07-01 13:37:00 +0200

    added try/catch block when calling the OpenWeather to log the error but continue handling the request

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Http;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +27,12 @@ class AppServiceProvider extends ServiceProvider
             'citizen' => 'App\Models\ProfileCitizen',
             'client' => 'App\Models\ProfileClient',
         ]);
+
+        Http::macro('openWeather', function () {
+            return Http::withQueryParameters([
+                'appi' => config('services.openweathermap.key'),
+                'units' => config('services.openweathermap.units'),
+            ])->baseUrl(config('services.openweathermap.url'));
+        });
     }
 }
